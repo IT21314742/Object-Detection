3e8d645dc9f9da57fdec99c545dbfc1a4abe9d75
IT21314742 <it21314742@my.sliit.lk>
2024-09-04 19:24:52 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
             cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
 
 for method in methods:
-    img2 = img2.copy()
+    img2 = img.copy()
 
-    result = cv2.matchTemplate(img2, template)
+    result = cv2.matchTemplate(img2, template, method)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
     print(min_loc,max_loc)
