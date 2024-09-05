d2d0e6fc91c12ac43eee60430bf4243016b649c5
IT21314742 <it21314742@my.sliit.lk>
2024-09-05 23:00:26 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ for method in methods:
 
 bottom_right = (location[0] + width, location[1] + height)
 cv2.rectangle(img, location, bottom_right, 255, 5)
-cv2.imshow('Mat', img2)
+cv2.imshow('Matc', img2)
 
