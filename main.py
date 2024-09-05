7670db37abf45fa3940c6dc5d6e4ed28be591381
IT21314742 <it21314742@my.sliit.lk>
2024-09-05 22:57:29 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -18,6 +18,6 @@ for method in methods:
     else:
         location = max_loc
 
-bottom_right = (location[0] + width, location[1] + heigh)
+bottom_right = (location[0] + width, location[1] + height)
 cv2.rectangle(img, location, )
 
